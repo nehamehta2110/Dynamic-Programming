5391943148bfe0971b126514c26d0586e5968828
nehamehta2110 <nainamehta2110@gmail.com>
2020-09-17 21:48:51 +0530

    added files

---
+++ LPS.py
@@ -0,0 +1,24 @@
+# code
+
+def lcs(a, b):
+    m = len(a)
+    n = len(b)
+    dp = [[0 for i in range(n + 1)] for j in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                dp[i][j] = 0
+
+            if a[i - 1] == b[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    return dp[m][n]
+
+
+for _ in range(int(input())):
+    a = str(input())
+    print(lcs(a, a[::-1]))
